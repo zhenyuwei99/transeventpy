e771b1fc72ba7fa60b0b6449efcc30d72ad25591
Zhenyu Wei <zhenyuwei@zhenyuwei-mbp.local>
2025-01-17 17:14:31 +0800

    Create the transeventpy package

---
+++ transeventpy/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Zhenyu Wei, Yitao Ge"
+__maintainer__ = "Zhenyu Wei, Yitao Ge"
+__copyright__ = "(C)Copyright 2024-present, Southeast University"
+__license__ = "MIT"
+
+
+import transeventpy.core as core
+import transeventpy.io as io
+import transeventpy.func as func
