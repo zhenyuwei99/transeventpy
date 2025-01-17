04b5a8394630ff6932ca34f0ae19a9056dc87217
Zhenyu Wei <zhenyuwei@zhenyuwei-mbp.local>
2025-01-17 17:11:54 +0800

    Create transeventpy.core.event module

---
+++ transeventpy/core/event.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+file : event.py
+created time : 2025/01/17
+author : Zhenyu Wei, YiTao Ge
+copyright : (C)Copyright 2024-present, Southeast University
+"""
+
+
+class Event:
+    def __init__(self):
+        pass
